8478aa62e9d3830ccabb5d16b13965a561f0b090
RodionAlekseev <144004822+RodionAlekseev@users.noreply.github.com>
2026-04-22 14:21:21 +0300

    Add files via upload

---
+++ Интернет-программирование/blog/src/test/java/com/Colibri_1/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Colibri_1.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
